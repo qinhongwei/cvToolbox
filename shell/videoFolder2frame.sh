14503fa8520e490b1756c4130cae00f6b56aeacd
Hongwei Qin <qinhongwei@users.noreply.github.com>
2016-08-04 12:04:30 +0800

    Update videoFolder2frame.sh

--- shell/videoFolder2frame.sh
+++ shell/videoFolder2frame.sh
@@ -3,5 +3,5 @@ for i in $(find -name "*.zip"); do unzip -od ${i%/*} $i; done
 # unrar rar file
 for i in $(find -name "*.rar"); do mkdir -p ${i%/*} && rar x $i ${i%/*}; done
 # find video file
-find -type f ! \( -name "*.zip" -o -name "*.rar" \) > all_video_list.txt
+find -type f ! \( -name "*.zip" -o -name "*.rar" -o -name "*.txt" -o -name "*.py" -o -name "*.sh" \) > all_video_list.txt
 python videolist2frame.py --video_list all_video_list.txt --save_dir frames
